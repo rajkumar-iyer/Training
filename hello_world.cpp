3953d17d3438320ce575c94f78840bb7a47217c7
rajkumar-iyer <iyer.rajkumar2@gmail.com>
2025-01-07 22:01:33 +0000

    Hello_world cpp program

---
+++ hello_world.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Library to handle input/output
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Print message to console
+    return 0;  // Return 0 to indicate successful execution
+}
